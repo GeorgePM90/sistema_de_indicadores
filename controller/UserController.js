6a0e43ceef878a4cdef8678e62bca4ed65937119
georgemendes <georgepmendes@gmail.com>
2020-02-09 21:39:35 -0300

    Atualizando o UserCOntroler

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,9 +1,18 @@
 class UserController {
-    constructor(formId){
+    constructor(formId, tableId){
         this.formEl = document.getElementById(formId)
+        this.tableEl = document.getElementById(tableId)
+        this.onSubmit()
     }
 
+    onSubmit(){
+        this.formEl.addEventListener("submit", (event)=>{
+            event.preventDefault()
+            this.addLine(this.getValues())
+        
+        })
 
+    }
 
     getValues(){
         let user = {}
@@ -20,7 +29,7 @@ class UserController {
             }
         })
     
-        var objectUser = new User(
+        return new User(
             user.name,
             user.gender,
             user.birth,
@@ -34,6 +43,26 @@ class UserController {
         )
 
 
+
+
+    }//fechando o metodo getValues
+
+    addLine(dataUser){
+
+            this.tableEl.innerHTML = `
+        
+                <tr>
+                    <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                    <td>${dataUser.name}</td>
+                    <td>${dataUser.email}</td>
+                    <td>${dataUser.admin}</td>
+                    <td>${dataUser.birth}</td>
+                        <td>
+                            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                        </td>
+                 </tr>   
+            `
 
 
     }
